0cb031ee7a64fd6685efe27a44139540ccc9122f
Bikem <bengisuozaydin@gmail.com>
2025-02-24 23:28:25 +0000

    add nix flake and deploy workflow

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Dessins";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rustPlatform = pkgs.rustPlatform;
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo rustc wasm-bindgen-cli miniserve
+          ];
+        };
+
+        packages.wasm = rustPlatform.buildRustPackage {
+          pname = "dessins";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoToml = ./crates/dessins/Cargo.toml;
+          cargoBuildTarget = "wasm32-unknown-unknown";
+          buildPhase = ''
+            cargo build --release --bin dessins --target wasm32-unknown-unknown
+            wasm-bindgen --out-name dessins \
+              --out-dir target \
+              --target web target/wasm32-unknown-unknown/release/dessins.wasm
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Rust WASM build with Nix";
+            license = licenses.mit;
+          };
+        };
+      }
+    );
+}
