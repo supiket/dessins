8dea052d2eb3b55d089c04881863c122dfd45a36
Bikem <bengisuozaydin@gmail.com>
2025-02-25 17:13:30 +0000

    optimize wasm build

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            cargo rustc wasm-bindgen-cli miniserve lld pkg-config
+            cargo rustc wasm-bindgen-cli lld pkg-config binaryen
           ];
         };
 
@@ -32,18 +32,40 @@
           cargoToml = ./crates/dessins/Cargo.toml;
           cargoBuildTarget = "wasm32-unknown-unknown";
 
-          nativeBuildInputs = with pkgs; [ wasm-bindgen-cli lld pkg-config];
+          nativeBuildInputs = with pkgs; [ wasm-bindgen-cli lld pkg-config binaryen ];
 
           buildPhase = ''
-            cargo build --release --bin dessins --target wasm32-unknown-unknown
+            set -e
+            mkdir -p target
+
+            cargo build --profile wasm-release --bin dessins --target wasm32-unknown-unknown --verbose
+
+            WASM_RELEASE_DIR="target/wasm32-unknown-unknown/wasm-release"
+            DESSINS_WASM=$WASM_RELEASE_DIR/dessins.wasm
+            DESSINS_BG_WASM=$WASM_RELEASE_DIR/dessins_bg.wasm
+            OPTIMIZED_DESSINS_BG_WASM=$WASM_RELEASE_DIR/dessins_bg.optimized.wasm
+
+            if [ ! -f $DESSINS_WASM ]; then
+                echo "error: $DESSINS_WASM not found"
+                exit 1
+            fi
+
             wasm-bindgen --out-name dessins \
-              --out-dir target \
-              --target web target/wasm32-unknown-unknown/release/dessins.wasm
+              --out-dir $WASM_RELEASE_DIR \
+              --target web $DESSINS_WASM
+
+            if [ ! -f $DESSINS_BG_WASM ]; then
+                echo "error: $DESSINS_BG_WASM not found after wasm-bindgen"
+                exit 1
+            fi
+
+            wasm-opt -Oz --output $OPTIMIZED_DESSINS_BG_WASM $DESSINS_BG_WASM
+            mv $OPTIMIZED_DESSINS_BG_WASM $DESSINS_BG_WASM
           '';
 
           installPhase = ''
             mkdir -p $out/target
-            cp -r target/* $out/target/
+            cp -r target/wasm32-unknown-unknown/wasm-release/* $out/target/
             cp index.html $out/target/
           '';
 
