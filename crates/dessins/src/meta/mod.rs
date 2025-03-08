2abfef8bb1eaaa8fc8ea5eaeb6b8c6a65fee346e
Bikem <bengisuozaydin@gmail.com>
2025-03-08 17:45:48 +0000

    remove ParamsMeta

--- crates/dessins/src/meta/mod.rs
+++ crates/dessins/src/meta/mod.rs
@@ -3,13 +3,9 @@ use bevy::reflect::Reflect;
 use expression_f32::ExpressionF32;
 use f32::F32;
 use nannou::prelude::*;
-use std::collections::HashMap;
 
 pub mod expression_f32;
 pub mod f32;
-
-#[derive(Clone, Debug, PartialEq, Reflect)]
-pub struct ParamsMeta(pub HashMap<String, ParamType>);
 
 #[derive(Clone, Debug, PartialEq, Reflect)]
 pub enum ParamType {
@@ -30,18 +26,5 @@ impl ControllableParam for ParamType {
             Self::F32(param) => param.toggle_animation_state(time),
             Self::ExpressionF32(param) => param.toggle_animation_state(time),
         }
-    }
-}
-
-impl core::ops::Deref for ParamsMeta {
-    type Target = HashMap<String, ParamType>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl core::ops::DerefMut for ParamsMeta {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
     }
 }
